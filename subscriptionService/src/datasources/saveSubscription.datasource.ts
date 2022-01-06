2d3ea94eebf7245ee56e89e5d839a5a78e60fb48
Davidian36 <davidiansdev@gmail.com>
2022-01-06 17:21:38 +0100

    new commit with tsconfigs

--- subscriptionService/src/datasources/saveSubscription.datasource.ts
+++ subscriptionService/src/datasources/saveSubscription.datasource.ts
@@ -30,20 +30,23 @@ export default class saveNewSubscription implements saveSubscriptionRepository {
         let query = 'INSERT INTO subscriptions (user_id,consent_flag,newsletter_id) SELECT ?, ?, ? WHERE NOT EXISTS(SELECT 1 FROM subscriptions WHERE user_id = ? AND consent_flag = ? AND newsletter_id = ?)'
         let subscriptionID = await db.insertQuery(query, [user_id, consent_flag, newsletter_id, user_id, consent_flag, newsletter_id])
 
-        // Make a request to the emailing docker linked service in order to notify the client
-        axios.post('http://mailing-container:8082/api/v1/mail/notify_subscription', {
-            subscribedClient: {
-                user_email: user_email,
-                user_firstName: user_firstName,
-                user_gender: user_gender,
-                user_birthDate: user_birthDate,
-                consent_flag: consent_flag,
-                newsletter_id: newsletter_id
-            }
-        })
-        .catch(error => {
-            logger.error(error)
-        })
+        // If there's a subscriptionID returned: make a request to the emailing docker linked service in order to notify the client
+        if(subscriptionID) {
+
+            axios.post('http://mailing-container:8082/api/v1/mail/notify_subscription', {
+                subscribedClient: {
+                    user_email: user_email,
+                    user_firstName: user_firstName,
+                    user_gender: user_gender,
+                    user_birthDate: user_birthDate,
+                    consent_flag: consent_flag,
+                    newsletter_id: newsletter_id
+                }
+            })
+            .catch(error => {
+                logger.error(error)
+            })
+        }
 
         return subscriptionID
     }
